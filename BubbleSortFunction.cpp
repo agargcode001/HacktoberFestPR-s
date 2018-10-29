a918f28cde0e4a84238b570d6a7d52cd32fbbd1c
GANK <32133617+agargcode001@users.noreply.github.com>
2018-10-30 00:45:57 +0530

    BubbleSortFunction.cpp
    Sorts Arrays

---
+++ BubbleSortFunction.cpp
@@ -0,0 +1,21 @@
+void BubbleSort(apvector <int> &num)
+{
+      int i, j, flag = 1;    // set flag to 1 to start first pass
+      int temp;             // holding variable
+      int numLength = num.length( ); 
+      for(i = 1; (i <= numLength) && flag; i++)
+     {
+          flag = 0;
+          for (j=0; j < (numLength -1); j++)
+         {
+               if (num[j+1] > num[j])      // ascending order simply changes to <
+              { 
+                    temp = num[j];             // swap elements
+                    num[j] = num[j+1];
+                    num[j+1] = temp;
+                    flag = 1;               // indicates that a swap occurred.
+               }
+          }
+     }
+     return;   //arrays are passed to functions by address; nothing is returned
+}
